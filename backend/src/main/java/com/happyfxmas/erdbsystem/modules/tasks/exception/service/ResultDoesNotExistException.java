c816df8378489189fb1cb8601dec8aa5dc050481
HappyMq <happy.xmas1@mail.ru>
2024-02-12 16:42:15 +0300

    created result exception

---
+++ backend/src/main/java/com/happyfxmas/erdbsystem/modules/tasks/exception/service/ResultDoesNotExistException.java
@@ -0,0 +1,8 @@
+package com.happyfxmas.erdbsystem.modules.tasks.exception.service;
+
+
+public class ResultDoesNotExistException extends RuntimeException {
+    public ResultDoesNotExistException(String message) {
+        super(message);
+    }
+}
