8e001f5b4efeb6c9293b89aec600f51de681c779
HappyMq <happy.xmas1@mail.ru>
2024-02-12 16:43:59 +0300

    extracted logic for handling exception validation

--- backend/src/main/java/com/happyfxmas/erdbsystem/exceptions/ApplicationExceptionHandler.java
+++ backend/src/main/java/com/happyfxmas/erdbsystem/exceptions/ApplicationExceptionHandler.java
@@ -3,8 +3,14 @@ package com.happyfxmas.erdbsystem.exceptions;
 import com.happyfxmas.erdbsystem.modules.ermodels.exception.validation.EnumValueException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice(basePackages = "com.happyfxmas.erdbsystem")
 public class ApplicationExceptionHandler {
@@ -23,6 +29,7 @@ public class ApplicationExceptionHandler {
 //        );
 //        return new ResponseEntity<>(exceptionDTO, HttpStatus.INTERNAL_SERVER_ERROR);
 //    }
+
     @ExceptionHandler(
             value = {
                     EnumValueException.class,
@@ -36,5 +43,29 @@ public class ApplicationExceptionHandler {
                 HttpStatus.INTERNAL_SERVER_ERROR.value()
         );
         return new ResponseEntity<>(enumException, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(
+            value = {
+                    MethodArgumentNotValidException.class
+            }
+    )
+    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        Map<String, List<String>> errorMap = methodArgumentNotValidException.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(
+                        Collectors.groupingBy(
+                                FieldError::getField,
+                                Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())
+                        )
+                );
+        var modelException = new ExceptionDTO(
+                errorMap,
+                methodArgumentNotValidException.getClass().getSimpleName(),
+                HttpStatus.BAD_REQUEST,
+                HttpStatus.BAD_REQUEST.value()
+        );
+        return new ResponseEntity<>(modelException, HttpStatus.BAD_REQUEST);
     }
 }
