2e5b03badfd8e948cc3ca48b56664dcf41852aab
HappyMq <happy.xmas1@mail.ru>
2024-02-12 16:40:22 +0300

    refactor exception handling

--- backend/src/main/java/com/happyfxmas/erdbsystem/modules/persons/exception/PersonExceptionHandler.java
+++ backend/src/main/java/com/happyfxmas/erdbsystem/modules/persons/exception/PersonExceptionHandler.java
@@ -1,47 +1,39 @@
 package com.happyfxmas.erdbsystem.modules.persons.exception;
 
 import com.happyfxmas.erdbsystem.exceptions.ExceptionDTO;
-import com.happyfxmas.erdbsystem.exceptions.NotFoundException;
-import com.happyfxmas.erdbsystem.exceptions.ServerException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.GroupNotFoundException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.GroupServerException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.PersonNotFoundException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.PersonServerException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.PositionNotFoundException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.PositionServerException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.StudentNotFoundException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.StudentServerException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.TeacherNotFoundException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.TeacherServerException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.UserNotFoundException;
-import com.happyfxmas.erdbsystem.modules.persons.exception.response.UserServerException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.GroupCreationException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.GroupDoesNotExistException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.PersonCreationException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.PersonDoesNotExistException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.PositionCreationException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.PositionDoesNotExistException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.StudentCreationException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.StudentDoesNotExistException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.TeacherCreationException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.TeacherDoesNotExistException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.UserCreationException;
+import com.happyfxmas.erdbsystem.modules.persons.exception.service.UserDoesNotExistException;
 import com.happyfxmas.erdbsystem.modules.persons.exception.validation.PersonValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-@RestControllerAdvice(basePackages = "com.ustu.erdbsystem.persons.api.controller")
+@RestControllerAdvice(basePackages = "com.happyfxmas.erdbsystem.modules.persons")
 public class PersonExceptionHandler {
 
     @ExceptionHandler(
             value = {
-                    PersonNotFoundException.class,
-                    UserNotFoundException.class,
-                    GroupNotFoundException.class,
-                    PositionNotFoundException.class,
-                    StudentNotFoundException.class,
-                    TeacherNotFoundException.class,
+                    PersonDoesNotExistException.class,
+                    UserDoesNotExistException.class,
+                    GroupDoesNotExistException.class,
+                    PositionDoesNotExistException.class,
+                    StudentDoesNotExistException.class,
+                    TeacherDoesNotExistException.class,
             }
     )
     public ResponseEntity<Object> handleNotFoundException(
-            NotFoundException notFoundException) {
+            RuntimeException notFoundException) {
         var personException = new ExceptionDTO(
                 notFoundException.getMessage(),
                 notFoundException.getClass().getSimpleName(),
@@ -53,16 +45,16 @@ public class PersonExceptionHandler {
 
     @ExceptionHandler(
             value = {
-                    PersonServerException.class,
+                    PersonCreationException.class,
                     PersonValidationException.class,
-                    UserServerException.class,
-                    GroupServerException.class,
-                    PositionServerException.class,
-                    StudentServerException.class,
-                    TeacherServerException.class,
+                    UserCreationException.class,
+                    GroupCreationException.class,
+                    PositionCreationException.class,
+                    StudentCreationException.class,
+                    TeacherCreationException.class,
             }
     )
-    public ResponseEntity<Object> handleServerException(ServerException serverException) {
+    public ResponseEntity<Object> handleServerException(RuntimeException serverException) {
         var personException = new ExceptionDTO(
                 serverException.getMessage(),
                 serverException.getClass().getSimpleName(),
@@ -70,28 +62,5 @@ public class PersonExceptionHandler {
                 HttpStatus.INTERNAL_SERVER_ERROR.value()
         );
         return new ResponseEntity<>(personException, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ExceptionHandler(
-            value = {
-                    MethodArgumentNotValidException.class
-            }
-    )
-    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException methodArgumentNotValidException) {
-        Map<String, List<String>> errorMap = methodArgumentNotValidException.getBindingResult().getFieldErrors()
-                .stream()
-                .collect(
-                        Collectors.groupingBy(
-                                FieldError::getField,
-                                Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())
-                        )
-                );
-        var personException = new ExceptionDTO(
-                errorMap,
-                methodArgumentNotValidException.getClass().getSimpleName(),
-                HttpStatus.BAD_REQUEST,
-                HttpStatus.BAD_REQUEST.value()
-        );
-        return new ResponseEntity<>(personException, HttpStatus.BAD_REQUEST);
     }
 }
