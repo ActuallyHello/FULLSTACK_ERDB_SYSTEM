0f3313291db9a9d4b201a93a64fd205cfd639a51
HappyMq <happy.xmas1@mail.ru>
2024-02-12 16:37:59 +0300

    enable auditing

--- backend/src/main/java/com/happyfxmas/erdbsystem/ErdbsystemApplication.java
+++ backend/src/main/java/com/happyfxmas/erdbsystem/ErdbsystemApplication.java
@@ -2,8 +2,12 @@ package com.happyfxmas.erdbsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ErdbsystemApplication {
 
 	public static void main(String[] args) {
